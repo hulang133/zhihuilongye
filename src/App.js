556491052135699d755a92d09a141fa18480e074
aniki <354651432@qq.com>
2020-11-08 12:05:00 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react"
+import * as api from "./api/index.js"
+import "bootstrap/dist/css/bootstrap.css"
+import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, ReferenceLine, ReferenceArea,
+  ReferenceDot, Tooltip, CartesianGrid, Legend, Brush, ErrorBar, AreaChart, Area,
+  Label, LabelList } from 'recharts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class extends React.Component {
+  state = {
+    client_id:0,
+    client_key:0,
+    data:{},
+  }
+
+  async componentDidMount() {
+    this.load()
+    setInterval(this.load, 3000)
+  }
+  load = async()=> {
+    const data = await api.data(this.state.client_id)
+
+
+    this.setState({data,client_key:data.client_key})
+  }
+  click = status=>()=>{
+    this.setState({client_key:status})
+    api.switches({
+      client_id:this.state.client_id,
+      status,
+      min_humi:0,
+      max_humi:0,
+    })
+
+  }
+  render(){
+    const it = this.state.data
+    return (
+      <div className="App">
+        <div className="container">
+          <ul className="info">
+            <li className="">air_temp:{it.air_temp}</li>
+            <li className="">air_humi:{it.air_humi}</li>
+            <li className="">lumen:{it.lumen}</li>
+            <li className="">soil_temp:{it.soil_temp}</li>
+            <li className="">soil_humi:{it.soil_humi}</li>
+           <li> {
+             this.state.client_key==0? (
+               <button className="btn btn-sm btn-primary" onClick={this.click(1)}>浇水</button>
+             ):(
+               <button className="btn btn-sm btn-primary" onClick={this.click(0)}>停止浇水</button>
+             )
+           }
+             </li>
+          </ul>
+        </div>
+        <div>
+          <LineChart data={[{a:100,b:20},{a:"fds",b:99}]} width="200" height="200">
+
+            <Line key="b" dataKey="b">
+            </Line>
+          </LineChart>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
---
+++ src/api/index.js
@@ -0,0 +1,23 @@
+import Axios from "axios"
+
+const axios = Axios.create({
+    baseURL:"http://139.196.141.151:83/",
+})
+
+export function data(id) {
+    return axios.get("/data/"+id).then(p=>{
+        if(p.data.length > 0) {
+            return p.data[0]
+        }
+
+        return null
+    })
+}
+
+export function history(id,begin,end) {
+    return axios.get(`/history/${id}?begin=${begin}&end=${end}`)
+}
+
+export function switches(data) {
+    return axios.post("/switches",data)
+}
